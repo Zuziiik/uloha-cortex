2c97ec2a40a569f4382c1f89343125293a635602
Zuzana Vilhelmova <vilhelmova.zuzana@gmail.com>
2016-05-08 19:04:13 +0200

    initial commit

---
+++ app/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+	require __DIR__ . '/../vendor/autoload.php';
+	$configurator = new Nette\Configurator;
+	$configurator->setDebugMode( TRUE ); // enable for your remote IP
+	$configurator->enableDebugger( __DIR__ . '/../log' );
+	$configurator->setTempDirectory( __DIR__ . '/../temp' );
+	$configurator->createRobotLoader()->addDirectory( __DIR__ )->register();
+	$configurator->addConfig( __DIR__ . '/config/config.neon' );
+	$configurator->addConfig( __DIR__ . '/config/config.local.neon' );
+	$container = $configurator->createContainer();
+
+	$container->getByType( 'Nette\Http\Response' )->warnOnBuffer = FALSE;
+	return $container;
---
+++ temp/cache/latte/uloha-cortex-app-presenter-templates-layout-latte-Template11a30a0971d92a0b21a10bb9d41357eb.php
@@ -0,0 +1,96 @@
+<?php
+// source: /var/www/uloha-cortex/app/presenter/templates/@layout.latte
+
+class Template11a30a0971d92a0b21a10bb9d41357eb extends Latte\Template {
+function render() {
+foreach ($this->params as $__k => $__v) $$__k = $__v; unset($__k, $__v);
+// prolog Latte\Macros\CoreMacros
+list($_b, $_g, $_l) = $template->initialize('9806827705', 'html')
+;
+// prolog Latte\Macros\BlockMacros
+//
+// block head
+//
+if (!function_exists($_b->blocks['head'][] = '_lb77f58ad359_head')) { function _lb77f58ad359_head($_b, $_args) { foreach ($_args as $__k => $__v) $$__k = $__v
+;
+}}
+
+//
+// block scripts
+//
+if (!function_exists($_b->blocks['scripts'][] = '_lb1fce37d291_scripts')) { function _lb1fce37d291_scripts($_b, $_args) { foreach ($_args as $__k => $__v) $$__k = $__v
+?>	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="//nette.github.io/resources/js/netteForms.min.js"></script>
+    	<script src="<?php echo Latte\Runtime\Filters::escapeHtml(Latte\Runtime\Filters::safeUrl($basePath), ENT_COMPAT) ?>/js/bootstrap.js"></script>
+        <script src="<?php echo Latte\Runtime\Filters::escapeHtml(Latte\Runtime\Filters::safeUrl($basePath), ENT_COMPAT) ?>/js/ie10-viewport-bug-workaround.js" async></script>
+
+    <!--[if lt IE 9]>
+        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+        <script type="text/javascript"
+              src="https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js"></script>
+    <![endif]-->
+<?php
+}}
+
+//
+// end of blocks
+//
+
+// template extending
+
+$_l->extends = empty($_g->extended) && isset($_control) && $_control instanceof Nette\Application\UI\Presenter ? $_control->findLayoutTemplateFile() : NULL; $_g->extended = TRUE;
+
+if ($_l->extends) { ob_start(function () {});}
+
+// prolog Nette\Bridges\ApplicationLatte\UIMacros
+
+// snippets support
+if (empty($_l->extends) && !empty($_control->snippetMode)) {
+	return Nette\Bridges\ApplicationLatte\UIRuntime::renderSnippets($_control, $_b, get_defined_vars());
+}
+
+//
+// main template
+//
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="">
+<?php if (isset($robots)) { ?>	<meta name="robots" content="<?php echo Latte\Runtime\Filters::escapeHtml($robots, ENT_COMPAT) ?>">
+<?php } ?>
+
+	<title><?php if (isset($_b->blocks["title"])) { ob_start(function () {}); Latte\Macros\BlockMacrosRuntime::callBlock($_b, 'title', $template->getParameters()); echo $template->striptags(ob_get_clean()) ?>
+ | <?php } ?></title>
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+	<link href="<?php echo Latte\Runtime\Filters::escapeHtml(Latte\Runtime\Filters::safeUrl($basePath), ENT_COMPAT) ?>/css/style.css" rel="stylesheet">
+	<link href="<?php echo Latte\Runtime\Filters::escapeHtml(Latte\Runtime\Filters::safeUrl($basePath), ENT_COMPAT) ?>/css/bootstrap.css"  rel="stylesheet" title="day-style">
+	<link href="<?php echo Latte\Runtime\Filters::escapeHtml(Latte\Runtime\Filters::safeUrl($basePath), ENT_COMPAT) ?>/css/bootstrap-theme.css" rel="stylesheet">
+		<link href="<?php echo Latte\Runtime\Filters::escapeHtml(Latte\Runtime\Filters::safeUrl($basePath), ENT_COMPAT) ?>/css/bootstrap-image-gallery.css" rel="stylesheet">
+	<link rel="shortcut icon" href="<?php echo Latte\Runtime\Filters::escapeHtml(Latte\Runtime\Filters::safeUrl($basePath), ENT_COMPAT) ?>/favicon.ico">
+	<link rel="stylesheet" type="text/css" href="https://code.jquery.com/ui/1.11.1/themes/smoothness/jquery-ui.css">
+
+
+	<?php if ($_l->extends) { ob_end_clean(); return $template->renderChildTemplate($_l->extends, get_defined_vars()); }
+call_user_func(reset($_b->blocks['head']), $_b, get_defined_vars())  ?>
+
+</head>
+
+<body>
+
+
+<?php $iterations = 0; foreach ($flashes as $flash) { ?>	<div<?php if ($_l->tmp = array_filter(array('flash', $flash->type))) echo ' class="', Latte\Runtime\Filters::escapeHtml(implode(" ", array_unique($_l->tmp)), ENT_COMPAT), '"' ?>
+><?php echo Latte\Runtime\Filters::escapeHtml($flash->message, ENT_NOQUOTES) ?></div>
+<?php $iterations++; } ?>
+        <div class='container'>
+<?php Latte\Macros\BlockMacrosRuntime::callBlock($_b, 'content', $template->getParameters()) ?>
+	    </div>
+
+
+
+<?php call_user_func(reset($_b->blocks['scripts']), $_b, get_defined_vars())  ?>
+</body>
+</html><?php
+}}
